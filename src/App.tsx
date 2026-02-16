a0d28965f5f23fc4f7fb18e82752f103a2c088b5
justinli34 <justinlibc@gmail.com>
2026-02-15 20:04:46 -0800

    refactor to named exports

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Scene from "./Scene";
+import { Scene } from "./Scene";
 import "./App.css";
 
-export default function App() {
+export const App = () => {
   return (
     <div className="container">
       <Scene />
       <h1 className="title">Resonatrics</h1>
     </div>
   );
-}
+};
--- src/Scene.tsx
+++ src/Scene.tsx
@@ -87,4 +87,4 @@ const Scene = () => {
   );
 };
 
-export default Scene;
+export { Scene };
